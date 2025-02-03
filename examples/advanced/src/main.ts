3e0e5ed0f0a93b78cc09d9eb4cd706c47ae267a4
Thada Wangthammang <thada.wth@gmail.com>
2025-02-03 20:10:14 +0700

    feat: support providers in terrakitStack

--- examples/advanced/src/main.ts
+++ examples/advanced/src/main.ts
@@ -1,12 +1,24 @@
-import { App } from "cdktf";
+import { App, TerraformStack, type IResolvable } from "cdktf";
 import { MyStack } from "./MyStack.js";
+import { AzurermProvider } from '../.gen/providers/azurerm/provider/index.js';
+
+/**
+ * @note Cannot register providers: Microsoft.AppConfiguration. Errors were: Cannot register provider Microsoft.AppConfiguration with Azure Resource Manager: unexpected status 403 (403 Forbidden) with error: AuthorizationFailed: The client 'your@example.com' with object id '00000000-0000-0000-0000-000000000000' does not have authorization to perform action 'Microsoft.AppConfiguration/register/action' over scope '/subscriptions/00000000-0000-0000-0000-000000000000' or the scope is invalid.
+ */
+const skipProviderRegistration: boolean | IResolvable = true;
 
 const app = new App();
+const defaultAzureProvider = new AzurermProvider(app, "azurerm_provider_default", {
+  skipProviderRegistration,
+});
 new MyStack(app, "demo-cdktf", {
   identifier: {
     env: 'prod',
     slot: 'prod',
     site: 'active'
+  },
+  providers: {
+    defaultAzureProvider
   }
 });
 app.synth();
--- src/TerrakitStack.ts
+++ src/TerrakitStack.ts
@@ -1,8 +1,14 @@
-import { App, TerraformStack } from "cdktf";
+import { App, TerraformStack, TerraformProvider } from "cdktf";
 import { Construct } from "constructs";
 
 export interface TerrakitOptions<Identifier extends object> {
-  identifier: Identifier;
+  identifier?: Identifier;
+  /**
+   * The providers to use in this stack.
+   * 
+   * @default - If not provided, the stack will use the default providers.
+   */
+  providers?: Record<string, TerraformProvider>;
 }
 
 export class TerrakitStack<Identifier extends object> extends TerraformStack {
