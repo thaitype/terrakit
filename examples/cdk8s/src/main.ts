bdf216019c3aae601e36c36aa79999e29ab2d5bc
Thada Wangthammang <thada.wth@gmail.com>
2025-02-08 21:57:28 +0700

    example: add example for cdk8s in deployment and service

--- examples/cdk8s/src/main.ts
+++ examples/cdk8s/src/main.ts
@@ -1,13 +1,52 @@
 import { Construct } from 'constructs';
 import { App, Chart, type ChartProps } from 'cdk8s';
+import * as k8s from '../imports/k8s.js';
 
 export class MyChart extends Chart {
-  constructor(scope: Construct, id: string, props: ChartProps = { }) {
+  constructor(scope: Construct, id: string, props: ChartProps = {}) {
     super(scope, id, props);
 
-    // define resources here
+
+    const label = { app: 'hello-k8s' };
+    this.createDeployment(label);
+    this.createService(label);
 
   }
+
+  createDeployment(label: any) {
+    new k8s.KubeDeployment(this, 'deployment', {
+      spec: {
+        replicas: 1,
+        selector: {
+          matchLabels: label
+        },
+        template: {
+          metadata: { labels: label },
+          spec: {
+            containers: [
+              {
+                name: 'web',
+                image: 'nginx',
+                ports: [{ containerPort: 80 }]
+              }
+            ]
+          }
+        }
+      }
+    });
+  }
+
+  createService(label: any) {
+    new k8s.KubeService(this, 'service', {
+      spec: {
+        type: 'LoadBalancer',
+        ports: [{ port: 80, targetPort: k8s.IntOrString.fromNumber(80) }],
+        selector: label
+      }
+    });
+  }
+
+
 }
 
 const app = new App();
